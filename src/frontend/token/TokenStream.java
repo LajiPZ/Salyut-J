7beb76ddf38e2f8c45a89411632fa0db9f134312
1540240353@qq.com <1540240353@qq.com>
2025-10-12 18:14:00 +0800

    fix

--- src/frontend/token/TokenStream.java
+++ src/frontend/token/TokenStream.java
@@ -78,40 +78,6 @@ public class TokenStream {
         this.index = this.checkpoint;
     }
 
-    public Token next(TokenType... types) {
-        if (check(types)) {
-            return tokens.poll();
-        } else {
-            throw new IllegalStateException("Expecting " + Arrays.toString(types) + "but got " + tokens.peek());
-        }
-    }
-
-    public boolean check(TokenType... tokenTypes) {
-        if (peek() == null) return false;
-        return peek().ofType(tokenTypes);
-    }
-
-    public boolean check(int offset,  TokenType... tokenTypes) {
-        if (peek() == null) return false;
-        return peek(offset).ofType(tokenTypes);
-    }
-
-    /**
-     * Check if the current token matches the given tokenTypes.
-     * If true, poll the current token, returns true, or false otherwise.
-     * This is especially useful for parsing optional elements in syntax.
-     * @param tokenTypes the specified token types
-     * @return result of comparison
-     */
-    public boolean checkPoll(TokenType... tokenTypes) {
-        if (check(tokenTypes)) {
-            poll();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     public boolean isEnd() {
         return index >= tokens.size();
     }
