52abeea8000af866de5db520a4b79375d6d21814
1540240353@qq.com <1540240353@qq.com>
2025-12-21 20:24:15 +0800

    feat: Oscillate

---
+++ src/frontend/llvm/optimization/Oscillate.java
@@ -0,0 +1,28 @@
+package frontend.llvm.optimization;
+
+import frontend.llvm.IrModule;
+import frontend.llvm.Pass;
+import frontend.llvm.analysis.ControlFlowAnalysis;
+import frontend.llvm.analysis.DominatorAnalysis;
+
+import java.util.List;
+
+public class Oscillate implements Pass {
+    @Override
+    public void run(IrModule module) {
+        for (int i = 0; i < 7; i++) {
+            List<Pass> passes = List.of(
+                new InlineFunction(),
+                new ConstantFolding(),
+                new ControlFlowAnalysis(),
+                new DominatorAnalysis(),
+
+                new SimplifyControlFlow(),
+                new ControlFlowAnalysis(),
+                new DominatorAnalysis(),
+                new EliminateDeadCode()
+            );
+            for (Pass p : passes) p.run(module);
+        }
+    }
+}
