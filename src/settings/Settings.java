73767546105af0d6b3cb57eaf3325475f748304c
1540240353@qq.com <1540240353@qq.com>
2026-01-04 20:14:14 +0800

    不想整了，就此结束

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -29,8 +29,8 @@ final public class Settings {
     public static class OptimizeConfig {
         public static boolean enableOptimization = true;
         public static boolean allowGlobalVarInCP1 = true;
-        public static boolean allowCallSaveToCP1 = false; // This is problematic, only use it when you need extra performance；在递归调用的时候就会发生问题
+        public static boolean allowCallSaveToCP1 = true; // This is problematic, only use it when you need extra performance；在递归调用的时候就会发生问题
         public static int maxLoopExtendIterations = 100;
-        public static int oscillateIterations = 0;
+        public static int oscillateIterations = 7;
     }
 }
